409563232a60492504cac1e963ab889ddb3253fe
alelabas <alelabas208@gmail.com>
2025-10-17 17:03:43 -0300

    change of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import Promo from "./components/Promo"
-import Disclaimer from "./pages/Disclaimer"
 import Footer from "./pages/Footer"
 import Navbar from "./pages/Navbar"
-import ProductsLargeList from "./Products/ProductsLargeList"
-import ProductsList from "./Products/ProductsList"
 
 const App = () => {
 	return (
 		<div className="w-full overflow-hidden">
 			<Navbar />
 			<main className="flex flex-col items-center justify-center gap-y-10">
-				<ProductsList />
-				<Promo />
-				<ProductsLargeList />
-				<Disclaimer />
 				<Footer />
 			</main>
 		</div>
