c482d38e907205ca62a3bc4043f275523b69717c
alelabas <alelabas208@gmail.com>
2025-10-23 13:09:59 -0300

    corrected bg image and styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./components/ScrollToTop";
+import "./styles.css";
 
 const App = () => {
   return (
-    <div className="flex min-h-screen w-full flex-col overflow-hidden bg-gray-900 bg-[url(bg-image/bg-page.png)] bg-cover bg-fixed bg-center text-white">
+    <div className="mainApp flex min-h-screen w-full flex-col overflow-hidden bg-gray-900 text-white">
       <Navbar />
       <ScrollToTop />
       <main className="flex min-h-screen flex-grow flex-col justify-center gap-y-10 p-10 pt-30">
