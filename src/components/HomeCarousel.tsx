84857c5c48c58a48524147687705486f4324f329
alelabas <alelabas208@gmail.com>
2025-10-23 00:23:04 -0300

    fixed images routes

--- src/components/HomeCarousel.tsx
+++ src/components/HomeCarousel.tsx
@@ -1,41 +1,41 @@
-import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel"
-import Autoplay from "embla-carousel-autoplay"
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 export const HomeCarousel = () => {
-	const images = {
-		1: "./src/assets/carousel/image1.jpg",
-		2: "./src/assets/carousel/image2.jpg",
-		3: "./src/assets/carousel/image3.jpg",
-		4: "./src/assets/carousel/image4.jpg",
-		5: "./src/assets/carousel/image5.jpg",
-		6: "./src/assets/carousel/image6.jpg",
-	}
+  const images = {
+    1: "@/assets/carousel/image1.jpg",
+    2: "@/assets/carousel/image2.jpg",
+    3: "@/assets/carousel/image3.jpg",
+    4: "@/assets/carousel/image4.jpg",
+    5: "@/assets/carousel/image5.jpg",
+    6: "@/assets/carousel/image6.jpg",
+  };
 
-	return (
-		<div>
-			<Carousel
-				opts={{
-					loop: true,
-				}}
-				plugins={[
-					Autoplay({
-						delay: 3000,
-					}),
-				]}
-				className="w-full h-120 text-blue-500 font-bold"
-			>
-				<CarouselContent>
-					{Object.values(images).map((src, index) => (
-						<CarouselItem key={index}>
-							<img
-								src={src}
-								alt={`Carousel image ${index + 1}`}
-								className="w-full h-120 object-contain"
-							/>
-						</CarouselItem>
-					))}
-				</CarouselContent>
-			</Carousel>
-		</div>
-	)
-}
+  return (
+    <div>
+      <Carousel
+        opts={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 3000,
+          }),
+        ]}
+        className="h-120 w-full font-bold text-blue-500"
+      >
+        <CarouselContent>
+          {Object.values(images).map((src, index) => (
+            <CarouselItem key={index}>
+              <img
+                src={src}
+                alt={`Carousel image ${index + 1}`}
+                className="h-120 w-full object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+};
