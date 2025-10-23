5312044d14f8ec07f5f579cbc8f5377876ce236d
alelabas <alelabas208@gmail.com>
2025-10-23 00:34:01 -0300

    add @vercel/analytics package and integrate analytics component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import { Outlet } from "react-router-dom"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-import ScrollToTop from "./components/ScrollToTop"
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/next";
 
 const App = () => {
-	return (
-		<div className="w-full overflow-hidden bg-gray-900 text-white min-h-screen flex flex-col bg-[url(./src/assets/bg-page.png)] bg-cover bg-fixed bg-center">
-			<Navbar />
-			<ScrollToTop />
-			<main className="flex flex-col justify-center gap-y-10 flex-grow p-10 min-h-screen pt-30">
-				<Outlet />
-			</main>
-			<Footer />
-		</div>
-	)
-}
+  return (
+    <div className="flex min-h-screen w-full flex-col overflow-hidden bg-gray-900 bg-[url(./src/assets/bg-page.png)] bg-cover bg-fixed bg-center text-white">
+      <Navbar />
+      <ScrollToTop />
+      <main className="flex min-h-screen flex-grow flex-col justify-center gap-y-10 p-10 pt-30">
+        <Outlet />
+      </main>
+      <Footer />
+      <Analytics />
+    </div>
+  );
+};
 
-export default App
+export default App;
